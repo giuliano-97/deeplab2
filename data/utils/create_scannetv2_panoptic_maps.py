8b1de17e7b7ecef02154eb3682f70ca406d1d080
Giuliano Albanese <albanesg@student.ethz.ch>
2021-11-01 12:14:28 +0100

    Parallelize create_scannetv2_panoptic_maps

--- data/utils/create_scannetv2_panoptic_maps.py
+++ data/utils/create_scannetv2_panoptic_maps.py
@@ -1,14 +1,17 @@
+import functools
 from pathlib import Path
 import numpy as np
 import pandas as pd
 
 from typing import Dict
 
+
 from absl import app
 from absl import flags
 from absl import logging
 from PIL import Image
 
+from joblib import Parallel, delayed
 
 FLAGS = flags.FLAGS
 
@@ -28,6 +31,8 @@ flags.DEFINE_string(
     default=None,
     help="Path to a directory containing scans as subdirs.",
 )
+
+flags.DEFINE_integer(name="jobs", default=2, help="Number of parallel jobs")
 
 # Same as Cityscapes
 _LABEL_DIVISOR = 255
@@ -69,9 +74,40 @@ def normalize_instance_map(instance_map: np.ndarray):
     return np.vectorize(conversion_dict.get)(instance_map)
 
 
-def create_deeplab2_panoptic_map(semantic_map: np.ndarray, instance_map: np.ndarray):
+def make_panoptic_from_semantic_and_instance(
+    semantic_map: np.ndarray,
+    instance_map: np.ndarray,
+):
     panoptic_map = semantic_map * _LABEL_DIVISOR + instance_map
     return panoptic_map.astype(np.int32)
+
+
+def generate_deeplab2_panoptic_map(
+    semantic_map_file_path: Path,
+    instance_map_file_path: Path,
+    panoptic_maps_dir_path: Path,
+    label_conversion_dict: Dict,
+):
+    semantic_map = np.array(Image.open(str(semantic_map_file_path)))
+    instance_map = np.array(Image.open(str(instance_map_file_path)))
+
+    # Convert semantic labels to the target labels set
+    converted_semantic_map = convert_semantic_map_labels(
+        semantic_map, label_conversion_dict
+    )
+
+    # Normalize the instance map so that all the instance ids are between 1 and #instances
+    normalized_instanced_map = normalize_instance_map(instance_map)
+
+    # Make panoptic map
+    panoptic_map = make_panoptic_from_semantic_and_instance(
+        converted_semantic_map, normalized_instanced_map
+    )
+
+    # Save panoptic map to disk
+    panoptic_map_file_path = panoptic_maps_dir_path / semantic_map_file_path.name
+    panoptic_map_image = Image.fromarray(panoptic_map)
+    panoptic_map_image.save(str(panoptic_map_file_path))
 
 
 def create_scannetv2_panoptic_maps(_):
@@ -80,6 +116,8 @@ def create_scannetv2_panoptic_maps(_):
     assert scans_root_dir_path.exists()
     labels_tsv_path = Path(FLAGS.labels_tsv_path)
     assert labels_tsv_path.exists()
+    n_jobs = FLAGS.jobs
+    assert n_jobs > 0
 
     # Load the labels conversion table - use the scannetv2 id as index
     label_conversion_master_table = pd.read_csv(str(labels_tsv_path), sep="\t")
@@ -110,35 +148,20 @@ def create_scannetv2_panoptic_maps(_):
 
         panoptic_maps_dir_path = scan_dir_path / "panoptic"
         panoptic_maps_dir_path.mkdir(exist_ok=True)
-        for semantic_map_file_path in semantic_maps_dir_path.glob("*.png"):
-            instance_map_file_path = (
-                instance_maps_dir_path / semantic_map_file_path.name
-            )
-            if not instance_map_file_path.exists():
-                continue
-            # Load semantic and instance map
-            semantic_map = np.array(Image.open(str(semantic_map_file_path)))
-            instance_map = np.array(Image.open(str(instance_map_file_path)))
 
-            # Convert the semantic labels
-            nyu40_semantic_map = convert_semantic_map_labels(
-                semantic_map, label_conversion_dict
-            )
+        semantic_map_files = sorted(list(semantic_maps_dir_path.glob("*.png")))
+        instance_map_files = sorted(list(instance_maps_dir_path.glob("*.png")))
 
-            # Normalize the instance map so that all the instance ids are between 1 and #instances
-            normalized_instanced_map = normalize_instance_map(instance_map)
+        job_fn = functools.partial(
+            generate_deeplab2_panoptic_map,
+            panoptic_maps_dir_path=panoptic_maps_dir_path,
+            label_conversion_dict=label_conversion_dict,
+        )
 
-            # Create panoptic map
-            panoptic_map = create_deeplab2_panoptic_map(
-                nyu40_semantic_map, normalized_instanced_map
-            )
-
-            # Save the panoptic map as png
-            panoptic_map_file_path = (
-                panoptic_maps_dir_path / semantic_map_file_path.name
-            )
-            panoptic_map_image = Image.fromarray(panoptic_map)
-            panoptic_map_image.save(str(panoptic_map_file_path))
+        Parallel(n_jobs=n_jobs)(
+            delayed(job_fn)(s, i)
+            for s, i in zip(semantic_map_files, instance_map_files)
+        )
 
 
 if __name__ == "__main__":
